b44f2498976a57f60792765132bf636f244079fe
Marjan112 <marjanramadanovic1234@gmail.com>
2025-11-04 23:38:34 +0100

    Rewrite in Python

---
+++ cpu_temp.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import psutil
+import os
+import time
+
+def find_tctl_temp():
+    temps = psutil.sensors_temperatures()
+    k10temp = temps["k10temp"]
+
+    for temp in k10temp:
+        if temp.label == "Tctl":
+            return int(temp.current * 1000)
+
+    return None
+
+def write_temp_to_file(tctl_temp: float, path: str):
+    file = open(path, "w")
+    file.write(f"{tctl_temp}")
+    file.close()
+
+home = os.environ["HOME"]
+
+while True:
+    tctl_temp = find_tctl_temp()
+    if tctl_temp is None:
+        exit(1)
+
+    write_temp_to_file(tctl_temp, f"{home}/.cpu_temp")
+    time.sleep(1)
